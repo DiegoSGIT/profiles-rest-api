bd69d0bc4acc4b636bfff0492402514b4aa52edf
DiegoSGIT <diego@dsanchez.localdomain>
2019-10-25 17:52:12 -0500

    All till permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
+from profiles_api import serializers, models
 class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
     """ Test APIView """
@@ -33,3 +34,51 @@ class HelloApiView(APIView):
     
     def delete(self, request, pk=None):
         return Response({"message": "Delete method"})
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(self, requets):
+        an_apiview = [
+            'Usess actions (list, create, retrieve, update, partialupdate)',
+            'Automatically maps to URLS usgun routes',
+            'More functionality without code'
+        ]
+        return Response({'message': 'Hello!', 'an_viewset': an_apiview})
+
+
+    def create(self, request):
+        """ Create a new create ViewSet """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello ' + name
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        return Response({'http-method': 'GET'})
+    
+    def update(self, request, pk=None):
+        return Response({'http-method': 'PUT'})
+        
+    def partial_update(self, request, pk=None):
+        return Response({'http-method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        return Response({'http-method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
+          
+         
+
+
+
+
