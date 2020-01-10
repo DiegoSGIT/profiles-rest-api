a9158db6882041c17956f8dd612aa193963592ae
DiegoSGIT <diego@dsanchez.localdomain>
2020-01-09 18:07:12 -0600

    Complete course without AWS

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,15 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers, models
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
+from profiles_api import serializers, models, permissions
+
 class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
     """ Test APIView """
@@ -73,12 +81,31 @@ class HelloViewSet(viewsets.ViewSet):
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    """ This filters allow us to avoid search manually """
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
-          
-         
+class UserLoginApiView(ObtainAuthToken):
+    """ Handle creating user authentication tokens """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """ Viewset to User profile feef """
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
 
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
 
+    def perform_create(self, serializer):
+        """ Overwrite the create method by django """         
+        serializer.save(user_profile=self.request.user)
